eda2db1c9fff68f0025bfc9727915547f142d3a0
Eric Wallen <ericwallen@Erics-MacBook-Pro.local>
2019-04-12 08:53:40 -0600

    Updates

---
+++ precache-manifest.0cc71b7a87628406e93b427f59445914.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a03d28a64c376bf7194",
+    "url": "/static/css/main.f5ed2d45.chunk.css"
+  },
+  {
+    "revision": "7a03d28a64c376bf7194",
+    "url": "/static/js/main.954f365a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b417f6a74b517fd518a",
+    "url": "/static/js/2.22fdb382.chunk.js"
+  },
+  {
+    "revision": "0c873d8b7d45690dd66d88bb55429475",
+    "url": "/static/media/kitchen.0c873d8b.jpg"
+  },
+  {
+    "revision": "44c76a62e5fba6e5c6f06659e843f949",
+    "url": "/static/media/logo.44c76a62.png"
+  },
+  {
+    "revision": "98d92a41ad3383f02b7eb5263977ff96",
+    "url": "/static/media/deck.98d92a41.jpg"
+  },
+  {
+    "revision": "94389e895bf5b3970618e9625810b134",
+    "url": "/static/media/exterior.94389e89.jpg"
+  },
+  {
+    "revision": "103026bf53f4b21e601215d49495c4cb",
+    "url": "/static/media/dinning.103026bf.jpg"
+  },
+  {
+    "revision": "1e42eec5ba572620d648f9bc99c7d3c5",
+    "url": "/index.html"
+  }
+];
